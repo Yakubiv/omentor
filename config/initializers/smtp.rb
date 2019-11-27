f386ef01ed92d07a41d25e521c2064af48f1b0bb
Petro <petro.yakubiv@gmail.com>
2019-11-27 17:01:30 +0200

    Update smtp settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 ActionMailer::Base.smtp_settings = {
-  domain: 'YOUR_DOMAIN.COM',
+  domain: 'http://studygo.com.ua',
   address: 'smtp.sendgrid.net',
-  port: 587,
+  port: 465,
   authentication: :plain,
+  enable_starttls_auto: true,
   user_name: 'apikey',
   password: ENV['SENDGRID_API_KEY']
 }
