2af827ba0d00917a0aa1c94680b28f845d098c11
Petro <petro.yakubiv@gmail.com>
2020-03-31 15:51:12 +0300

    Fx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://studygo.com.ua' }
-  config.force_ssl = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -50,7 +49,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
