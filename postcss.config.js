29701201dcda53040c19395104e9010c8a680f82
Petro <petro.yakubiv@gmail.com>
2020-05-03 19:06:04 +0300

    Add purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './app/**/*.html.erb',
+    './app/**/*.html.haml',
+    './app/**/*.haml',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.jsx',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -9,6 +24,9 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
