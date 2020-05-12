7d823d3fd8fa9f0e0e6bb58c0f00b31f6530c4af
Petro <petro.yakubiv@gmail.com>
2020-05-12 16:33:05 +0300

    add ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
                                                  'http://studygo.com.ua']
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'http://studygo.com.ua' }
+  config.action_mailer.default_url_options = { host: 'https://studygo.com.ua' }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
