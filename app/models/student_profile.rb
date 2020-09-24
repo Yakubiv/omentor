2bd99bbbc51359a7914955b81d7e3876886c53c7
Petro <petro.yakubiv@gmail.com>
2020-09-24 11:59:01 +0300

    Fix association

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -5,7 +5,7 @@ class StudentProfile < Profile
   has_many :tutor_profiles, through: :class_rooms, dependent: :destroy
   has_many :lessons, through: :class_rooms
   has_many :favorites, foreign_key: :profile_id
-  has_many :favorite_tutor_profiles, through: :favorites, source: :favoritable, source_type: 'TutorProfile'
+  has_many :favorite_tutor_profiles, through: :favorites, source: :favoritable, source_type: 'Profile'
 
   def favorite?(favoritable)
     favorites.where(
