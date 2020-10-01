0ec6e54392f77a48d540a598e6cb2e3a59f84c04
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-10-01 11:06:36 +0300

    Test Favorite model

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:profile) { create(:favorite, profile: profile) }
+  it "should require the profile id to be unique" do
+    Favorite.create(:favorite, profile: profile)
+    duplicate = build(:favorite, profile: profile)
+    expect(duplicate.valid?).to be_falsy
+  end
 end
