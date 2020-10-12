f96464475ee155a4a6f8546ec00606620ea18c48
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-10-12 16:01:31 +0300

    Add validate to Speak model

--- app/models/speak.rb
+++ app/models/speak.rb
@@ -1,5 +1,6 @@
 class Speak < ApplicationRecord
     belongs_to :tutor_profile, foreign_key: :profile_id
     belongs_to :language, foreign_key: :language_id
+    validates :language_id, uniqueness: { scope: :profile_id }
     enum level: %i[beginer elementary intermediate upper-intermediate advanced proficiency native]
 end
