fbf5ef04e530dd3aa89e369641209f92f52a804d
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-10-26 18:17:19 +0200

    Tutor can save students to favorite list

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -6,11 +6,4 @@ class StudentProfile < Profile
   has_many :lessons, through: :class_rooms
   has_many :favorites, foreign_key: :profile_id
   has_many :favorite_tutor_profiles, through: :favorites, source: :favoritable, source_type: 'Profile'
-
-  def favorite?(favoritable)
-    favorites.where(
-      favoritable_id: favoritable.id,
-      favoritable_type: favoritable.class.base_class.name
-    ).size.positive?
-  end
 end
