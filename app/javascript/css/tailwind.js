42cf4e76dd17628cc7c28c1d2ebe34d74e84ce0d
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-10-27 20:37:43 +0200

    Change profile_card for chats

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -199,6 +199,7 @@ module.exports = {
       inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
       outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
       none: "none",
+      solid: "0px 0px 0px 2px #ffffff;",
     },
     container: {},
     cursor: {
