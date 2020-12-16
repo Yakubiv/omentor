0b34d2ec902cfa4edaa5ea88e5d69fb1ac070169
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-12-16 15:39:15 +0200

    Add search messages for chat student with tutors

--- app/controllers/students/chats_controller.rb
+++ app/controllers/students/chats_controller.rb
@@ -1,20 +1,30 @@
 # frozen_string_literal: true
 
 class Students::ChatsController < Students::BaseController
-  before_action :load_class_rooms, only: %i[index show]
+  before_action :load_class_rooms, only: %i[show]
 
   def index
-
+    @class_rooms = ChatQuery.new(query_params).results
   end
 
   def show
     @class_room = ClassRoom.includes(:messages, :student_profile, :tutor_profile).find_by(uuid: params[:id])
-    @messages = @class_room.messages
+    @messages = MessageQuery.new(message_search_params).results
 
     @messages.my_unread_messages(current_student_profile).update_all(read: true)
   end
 
   private
+
+  attr_reader :class_room
+
+  def query_params
+    { search_string: params[:search], type: "tutor", current_profile: current_student_profile }
+  end
+
+  def message_search_params
+    { class_room: class_room, search_string: params[:search] }
+  end
 
   def load_class_rooms
     @class_rooms = current_student_profile.class_rooms.includes(:tutor_profile)
