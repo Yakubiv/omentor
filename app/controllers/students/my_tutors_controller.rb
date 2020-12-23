e66329d68478fc289251e20461be0753137eb853
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-12-23 16:31:54 +0200

    Link from tutor_profile to chat

--- app/controllers/students/my_tutors_controller.rb
+++ app/controllers/students/my_tutors_controller.rb
@@ -3,6 +3,7 @@
 class Students::MyTutorsController < Students::BaseController
   def index
     @tutors = ProfilesQuery.new(query_params).results
+    @students_chat_ids ||= current_student_profile.class_rooms.pluck(:tutor_profile_id, :uuid).to_h
   end
 
   def show
