bb9924dc45b90b6bb658a19b8e67830d18146758
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2021-02-10 20:19:09 +0200

    wip: add el-transition to modals calendar

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus";
+import { enter, leave } from "el-transition";
 
 export default class extends Controller {
   static targets = ["overlay", "panel"];
@@ -9,52 +10,13 @@ export default class extends Controller {
 
   open() {
     this.element.classList.remove("invisible");
-    this.overlayTarget.classList.remove("opacity-0", "ease-in", "duration-200");
-    this.panelTarget.classList.remove(
-      "ease-in",
-      "duration-200",
-      "opacity-0",
-      "translate-y-4",
-      "sm:translate-y-0",
-      "sm:scale-95"
-    );
-
-    this.element.classList.add("visible");
-    this.overlayTarget.classList.add("opacity-75", "ease-out", "duration-300");
-    this.panelTarget.classList.add(
-      "ease-out",
-      "duration-300",
-      "opacity-100",
-      "translate-y-0",
-      "sm:scale-100"
-    );
+    enter(this.overlayTarget);
+    enter(this.panelTarget);
   }
 
   close() {
-    this.element.classList.remove("visible");
-    this.overlayTarget.classList.remove(
-      "opacity-75",
-      "ease-out",
-      "duration-300"
-    );
-    this.panelTarget.classList.remove(
-      "ease-out",
-      "duration-300",
-      "opacity-100",
-      "translate-y-0",
-      "sm:scale-100"
-    );
-
-    this.element.classList.add("invisible");
-    this.overlayTarget.classList.add("opacity-0", "ease-in", "duration-200");
-    this.panelTarget.classList.add(
-      "ease-in",
-      "duration-200",
-      "opacity-0",
-      "translate-y-4",
-      "sm:translate-y-0",
-      "sm:scale-95"
-    );
     this.element.remove();
+    leave(this.overlayTarget);
+    leave(this.panelTarget);
   }
 }
