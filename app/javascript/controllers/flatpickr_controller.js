b1d1c53c126d33db52c1ee467ebc3e72dba911e0
Petro <petro.yakubiv@gmail.com>
2021-02-10 13:45:44 +0200

    wip: datepicker

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,15 +1,19 @@
 import Flatpickr from "stimulus-flatpickr";
+import { Ukrainian } from "flatpickr/dist/l10n/uk.js";
 import "flatpickr/dist/themes/airbnb.css";
 
 export default class extends Flatpickr {
   connect() {
+    console.log('hihih');
+
     this.config = {
       ...this.config,
       altInput: true,
+      allowInput: true,
       altInputClass: "",
-      altFormat: "D, M j, Y"
+      enableTime: true,
+      locale: Ukrainian,
     };
-
     super.connect();
   }
 }
